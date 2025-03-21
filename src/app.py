1bfe5519e2b2003ba9d7b36e1770f7b9ce26be20
lymnah <charly@mancel.net>
2025-03-21 17:19:30 +0100

    feat: ui update css

--- src/app.py
+++ src/app.py
@@ -19,15 +19,13 @@ st.title("INOX Tender AI - Assistance aux Appels d'Offres")
 # Render the sidebar directly in app.py
 with st.sidebar:
     your_company_logo = load_image_as_base64("resources/your_company_logo.png")
-    client_company_logo = load_image_as_base64("resources/client_company_logo.png")
 
     # Display logos at the top of the sidebar
-    if your_company_logo and client_company_logo:
+    if your_company_logo:
         st.markdown(
             f"""
             <div class="sidebar-logo-container">
                 <img src="{your_company_logo}" class="sidebar-logo" alt="Your Company Logo">
-                <img src="{client_company_logo}" class="sidebar-logo" alt="Client Company Logo">
             </div>
             """,
             unsafe_allow_html=True,
